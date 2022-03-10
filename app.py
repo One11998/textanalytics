c258e8087692e6ef1fc1e8984c0118f11d23dac4
One11998 <99379517+One11998@users.noreply.github.com>
2022-03-10 11:18:56 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask, request, render_template
+
+
+# In[2]:
+
+
+app = Flask(__name__)
+
+
+# In[3]:
+
+
+get_ipython().system('pip install textblob')
+
+
+# In[4]:
+
+
+
+from textblob import TextBlob
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    if request.method =="POST":
+        t = request.form.get("text")
+        print(t)
+        r = TextBlob(t).sentiment
+        return(render_template("index.html", result = r))
+    else:
+        return(render_template("index.html", result = "2"))
+
+
+# In[5]:
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
